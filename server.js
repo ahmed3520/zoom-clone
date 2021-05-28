d97167bb177da111d931ce3f7b86733da3052045
ahmed3520 <ataha352000@gmail.com>
2021-05-28 10:28:42 +0200

    initalizing the project

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const {v4: uuidV4} = require('uuid')
+console.log("lool")
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.get('/',(req, res)=>{
+res.redirect(`/${uuidV4()}`)
+})
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId: req.params.room})
+})
+server.listen(7000);
